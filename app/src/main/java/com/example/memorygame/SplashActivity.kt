76fae3820dab058c5311d5ea7a819741ffadd373
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-09-04 08:47:00 +0530

    splash screen added

---
+++ app/src/main/java/com/example/memorygame/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.memorygame
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+
+class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2500)
+
+    }
+}
---
+++ app/src/main/java/com/example/memorygame/models/UserImageList.kt
@@ -0,0 +1,5 @@
+package com.example.memorygame.models
+
+import com.google.firebase.firestore.PropertyName
+
+data class UserImageList(@PropertyName("images") val images: List<String>? = null)
